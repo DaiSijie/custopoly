400e5fd3dc802bbb7e8b7dab4eea7595d9e90406
Gilbert Maystre <gilbert@maystre.ch>
2018-04-04 13:27:03 +0200

    First commit

---
+++ src/ch/maystre/gilbert/custopoly/graphics/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Gilbert Maystre
+ * 04.04.18
+ */
+
+package ch.maystre.gilbert.custopoly.graphics;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello, nothing implemented yet!");
+    }
+
+}
