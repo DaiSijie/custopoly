cc0df525aa0f4eda66b82e61eb0fcee746eb344e
Gilbert Maystre <gilbert@maystre.ch>
2018-04-04 18:34:35 +0200

    First details of location card

--- src/ch/maystre/gilbert/custopoly/graphics/GraphicalObject.java
+++ src/ch/maystre/gilbert/custopoly/graphics/GraphicalObject.java
@@ -8,26 +8,73 @@ package ch.maystre.gilbert.custopoly.graphics;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public abstract class GraphicalObject {
+
+    private static Font boldFont;
+    private static Font regularFont;
 
     protected final BufferedImage image;
     protected final Graphics2D g;
 
     public GraphicalObject(int width, int height){
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g = image.createGraphics();
+        g = getGraphics();
+        adjustFont(true, 13f);
     }
+
+    private Graphics2D getGraphics(){
+        Graphics2D toReturn = image.createGraphics();
+
+        // set rendering hints
+        toReturn.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        toReturn.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        return toReturn;
+    }
+
+    /**
+     * Adjusts the font in monopoly style
+     */
+    protected void adjustFont(boolean bold, float size){
+        if(boldFont == null || regularFont == null){
+            try {
+                InputStream stream = new FileInputStream("/Users/gilbert/Programming/java-intelij/custopoly/res/fonts/kabel.ttf");
+                boldFont = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(13f);
+                stream.close();
+                stream = new FileInputStream("/Users/gilbert/Programming/java-intelij/custopoly/res/fonts/Monopoly Regular.ttf");
+                regularFont = new Font("Arial", Font.PLAIN, 13);;
+                stream.close();
+            } catch (IOException | FontFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Font toPut = (bold ? boldFont : regularFont).deriveFont(size);
+        g.setFont(toPut);
+    }
+
 
     public abstract BufferedImage buildImage();
 
-    protected void drawCentered(Graphics2D g, String text, Rectangle2D box) {
+    protected void drawCentered(String text, Rectangle2D box) {
         FontMetrics metrics = g.getFontMetrics(g.getFont());
 
         double x = box.getX() + (box.getWidth() - metrics.stringWidth(text)) / 2;
         double y = box.getY() + ((box.getHeight() - metrics.getHeight()) / 2) + metrics.getAscent();
 
         g.drawString(text, (int) x, (int) y);
+    }
+
+    protected void drawLeft(String text, Rectangle2D box){
+        // todo
+    }
+
+    protected void drawRight(String text, Rectangle2D box){
+        // todo
     }
 
 }
--- src/ch/maystre/gilbert/custopoly/graphics/location/CardLocation.java
+++ src/ch/maystre/gilbert/custopoly/graphics/location/CardLocation.java
@@ -16,15 +16,29 @@ public class CardLocation extends Location {
 
     // drawing constants
 
-    private static final int WIDTH = 100;
+    private static final int WIDTH = 300;
 
-    private static final int HEIGHT = 150;
+    private static final int HEIGHT = 450;
 
-    private static final int OUTER_MARGIN = 5;
+    private static final int OUTER_MARGIN = 10;
 
-    private static final int INNER_MARGIN = 3;
+    private static final int INNER_MARGIN = 5;
+
+
+    // title
+
+    private static final int OVER_TITLE_HEIGHT = 30;
+
+    private static final int TITLE_MARGIN = 3;
 
     private static final int TITLE_HEIGHT = 30;
+
+    // copyright box
+    private static final int COPYRIGHT_WIDTH = 100;
+
+    private static final int COPYRIGHT_HEIGHT = 15;
+
+    private static final int COPYRIGHT_BOTTOM_DIST = 3;
 
     public CardLocation(String name, int rank){
         super(name, rank, WIDTH, HEIGHT);
@@ -38,17 +52,39 @@ public class CardLocation extends Location {
 
         /* 2: borders */
         g.setColor(Color.BLACK);
-        g.drawLine(OUTER_MARGIN, OUTER_MARGIN, OUTER_MARGIN, HEIGHT - OUTER_MARGIN);
-        g.drawLine(OUTER_MARGIN, OUTER_MARGIN, WIDTH - OUTER_MARGIN, OUTER_MARGIN);
-        g.drawLine(WIDTH - OUTER_MARGIN, OUTER_MARGIN, WIDTH - OUTER_MARGIN, HEIGHT - OUTER_MARGIN);
+        Rectangle2D.Double borderBox = new Rectangle2D.Double(OUTER_MARGIN, OUTER_MARGIN, WIDTH - 2 * OUTER_MARGIN, HEIGHT - 2 * OUTER_MARGIN);
+        g.draw(borderBox);
 
         /* 3: title */
         int totMargin = OUTER_MARGIN + INNER_MARGIN;
-        Rectangle2D.Double titleBox = new Rectangle2D.Double(totMargin, totMargin, WIDTH - 2 * totMargin, TITLE_HEIGHT);
+        Rectangle2D.Double titleBox = new Rectangle2D.Double(totMargin, totMargin, WIDTH - 2 * totMargin, OVER_TITLE_HEIGHT + 2 * TITLE_HEIGHT + TITLE_MARGIN);
         g.setColor(Color.GREEN);
         g.fill(titleBox);
         g.setColor(Color.BLACK);
-        drawCentered(g, name, titleBox);
+        g.draw(titleBox);
+
+        Rectangle2D.Double t1Box = new Rectangle2D.Double(titleBox.x, titleBox.y, titleBox.width, OVER_TITLE_HEIGHT);
+        adjustFont(false, 18);
+        drawCentered("Titre de propriété", t1Box);
+
+
+        Rectangle2D.Double t2Box = new Rectangle2D.Double(titleBox.x, titleBox.y + OVER_TITLE_HEIGHT + TITLE_MARGIN, titleBox.width, TITLE_HEIGHT);
+        adjustFont(true, 24);
+        drawCentered("AVENUE", t2Box);
+
+        Rectangle2D.Double t3Box = new Rectangle2D.Double(titleBox.x, titleBox.y + OVER_TITLE_HEIGHT + TITLE_MARGIN + TITLE_HEIGHT, titleBox.width, TITLE_HEIGHT);
+        drawCentered("DE LA CALIFORNIE", t3Box);
+
+        /* 4: copyright text */
+        double copyrightStartX = (WIDTH - COPYRIGHT_WIDTH) / 2.;
+        double copyrightStartY = HEIGHT - COPYRIGHT_HEIGHT - COPYRIGHT_BOTTOM_DIST;
+        Rectangle2D.Double copyrightBox = new Rectangle2D.Double(copyrightStartX, copyrightStartY, COPYRIGHT_WIDTH, COPYRIGHT_HEIGHT);
+        g.setColor(Color.WHITE);
+        g.fill(copyrightBox);
+        g.setColor(Color.BLACK);
+        adjustFont(true, 8f);
+        drawCentered("Copyright G.M 2018", copyrightBox);
+
 
         return image;
     }
